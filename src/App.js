a5935a05d0bfb3e542f11d48dbea67ea490301f1
Vertipae <33002993+Vertipae@users.noreply.github.com>
2019-06-20 12:26:19 +0300

    >>(//////)^>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+// import UserItem from "./components/users/UserItem";
+import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  // async componentDidMount() {
+  //   // console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
+  //   this.setState({ loading: true });
+
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${
+  //       process.env.REACT_APP_GITHUB_CLIENT_ID
+  //     }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  async componentDidMount() {
+    // console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
+    this.setState({ loading: true });
+
+    const res = await axios.get("https://api.github.com/users/vertipae");
+    // console.log(res);
+    // [] <- Changes this to list so it can map through or Array.isArray(res.data) ? res.data : [res.data] <- Checks if array or not to be array
+    this.setState({ users: [res.data], loading: false });
+  }
+
+  // Search Github users
+  searchUsers = async text => {
+    // console.log(text);
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        {/* title='Github Finder' icon='fab fa-github' */}
+        <Navbar />
+        <div className='container'>
+          <Search searchUsers={this.searchUsers} />
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/users/Search.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export class Search extends Component {
+  state = {
+    text: ""
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    // Passing this to App.js
+    this.props.searchUsers(this.state.text);
+    // Clearing form
+    this.setState({ text: "" });
+  };
+
+  onChange = e => {
+    // this.setState({ text: e.target.value });
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit} className='form'>
+          <input
+            type='text'
+            name='text'
+            placeholder='Search Users..'
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input
+            type='submit'
+            value='Search'
+            className='btn btn-dark btn-block'
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
