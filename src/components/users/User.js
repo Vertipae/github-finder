6aac374aca25a9873599a4f1d28b0bb1fe1b4807
Vertipae <33002993+Vertipae@users.noreply.github.com>
2019-07-11 15:35:07 +0300

    Add single user component & layout ٩(•̮̮̃•̃)۶

---
+++ src/components/users/User.js
@@ -0,0 +1,112 @@
+import React, { Component, Fragment } from "react";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+export class User extends Component {
+  // :login from App.js (match.params)
+  // App.js getUser method for making request to get the user information and after that filling the user state with the response aaand then passing the user state to user component. Simple.
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login);
+  }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired
+  };
+
+  render() {
+    console.log(this.props.user);
+    const {
+      name,
+      avatar_url,
+      location,
+      bio,
+      company,
+      blog,
+      login,
+      html_url,
+      followers,
+      following,
+      public_repos,
+      public_gists,
+      hireable
+    } = this.props.user;
+
+    const { loading } = this.props;
+    // If loading prop is true then show spinner
+    if (loading) return <Spinner />;
+
+    return (
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          Back to Search
+        </Link>
+        Hireable:{" "}
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              className='round-img'
+              alt=''
+              style={{ width: "150px" }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {/* If there is a bio then shows the bio */}
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className='btn btn-dark my-1'>
+              {login} Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-light'>Public repos: {public_repos}</div>
+          <div className='badge badge-dark'>Public gists: {public_gists}</div>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default User;
